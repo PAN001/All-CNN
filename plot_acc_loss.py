b06121b3466cf3aca1750ed374d647e921fbb871
PAN001 <pan.atopos@gmail.com>
2017-10-26 18:14:25 -0400

    update

--- plot_acc_loss.py
+++ plot_acc_loss.py
@@ -121,62 +121,121 @@ import pickle
 # # plt.show()
 # plt.savefig("exp1_loss.png")
 
-# exp2
-id = "LSUV_nopp"
-accs_batch_path_LSUV_nopp = id + "/" + "all_cnn_accs_batch_" + id + ".acc"
-losses_batch_path_LSUV_nopp = id + "/" + "all_cnn_losses_batch_" + id + ".loss"
+# # exp2
+# id = "LSUV_nopp"
+# accs_batch_path_LSUV_nopp = id + "/" + "all_cnn_accs_batch_" + id + ".acc"
+# losses_batch_path_LSUV_nopp = id + "/" + "all_cnn_losses_batch_" + id + ".loss"
+#
+# id = "LSUV_norm_shift_flip"
+# accs_batch_path_LSUV_nsf = id + "/" + "all_cnn_accs_batch_" + id + ".acc"
+# losses_batch_path_LSUV_nsf = id + "/" + "all_cnn_losses_batch_" + id + ".loss"
+#
+# id = "LSUV_norm_shift_flip_zca"
+# accs_batch_path_LSUV_norm_shift_flip_zca = id + "/" + "all_cnn_accs_batch_" + id + ".acc"
+# losses_batch_path_LSUV_norm_shift_flip_zca = id + "/" + "all_cnn_losses_batch_" + id + ".loss"
+#
+#
+# with open(accs_batch_path_LSUV_nopp, "rb") as fp:
+#     accs_batch_LSUV_nopp = pickle.load(fp)
+#
+# with open(losses_batch_path_LSUV_nopp, "rb") as fp:
+#     losses_batch_LSUV_nopp = pickle.load(fp)
+#
+# with open(accs_batch_path_LSUV_nsf, "rb") as fp:
+#     accs_batch_LSUV_nsf = pickle.load(fp)
+#
+# with open(losses_batch_path_LSUV_nsf, "rb") as fp:
+#     losses_batch_LSUV_nsf = pickle.load(fp)
+#
+# with open(accs_batch_path_LSUV_norm_shift_flip_zca, "rb") as fp:
+#     accs_batch_LSUV_norm_shift_flip_zca = pickle.load(fp)
+#
+# with open(losses_batch_path_LSUV_norm_shift_flip_zca, "rb") as fp:
+#     losses_batch_LSUV_norm_shift_flip_zca = pickle.load(fp)
+#
+# # acc
+# fig = plt.figure(1, figsize=(40, 10))
+# plt.plot(range(0, len(accs_batch_LSUV_nopp))[0:-1:5], accs_batch_LSUV_nopp[0:-1:5])
+# plt.plot(range(0, len(accs_batch_LSUV_nsf))[0:-1:5], accs_batch_LSUV_nsf[0:-1:5])
+# plt.plot(range(0, len(accs_batch_LSUV_norm_shift_flip_zca))[0:-1:5], accs_batch_LSUV_norm_shift_flip_zca[0:-1:5])
+# # plt.xticks(range(0, len(accs_batch_LSUV_nopp))[0:-1:5], range(0, len(accs_batch_LSUV_nopp))[0:-1:500])
+# plt.title('Exp2: model accuracy per batch')
+# plt.ylabel('acc')
+# plt.xlabel('batch')
+# plt.legend(['no preprocessing', 'shift, flip and normalization', 'shift, flip, normalization and zca whitening'], loc='upper left')
+# # plt.show()
+# plt.savefig("exp2_acc.png")
+#
+#
+# # loss
+# fig = plt.figure(2, figsize=(40, 10))
+# plt.plot(range(0, len(losses_batch_LSUV_nopp))[0:-1:5], losses_batch_LSUV_nopp[0:-1:5])
+# plt.plot(range(0, len(losses_batch_LSUV_nsf))[0:-1:5], losses_batch_LSUV_nsf[0:-1:5])
+# plt.plot(range(0, len(losses_batch_LSUV_norm_shift_flip_zca))[0:-1:5], losses_batch_LSUV_norm_shift_flip_zca[0:-1:5])
+# # plt.xticks(range(0, len(losses_batch_LSUV_nopp))[0:-1:5], range(0, len(losses_batch_LSUV_nopp))[0:-1:5])
+# plt.title('Exp2: model loss per batch')
+# plt.ylabel('loss')
+# plt.xlabel('batch')
+# plt.legend(['no preprocessing', 'shift, flip and normalization', 'shift, flip, normalization and zca whitening'], loc='upper left')
+# # plt.show()
+# plt.savefig("exp2_loss.png")
 
-id = "LSUV_norm_shift_flip"
-accs_batch_path_LSUV_nsf = id + "/" + "all_cnn_accs_batch_" + id + ".acc"
-losses_batch_path_LSUV_nsf = id + "/" + "all_cnn_losses_batch_" + id + ".loss"
 
-id = "LSUV_norm_shift_flip_zca"
-accs_batch_path_LSUV_norm_shift_flip_zca = id + "/" + "all_cnn_accs_batch_" + id + ".acc"
-losses_batch_path_LSUV_norm_shift_flip_zca = id + "/" + "all_cnn_losses_batch_" + id + ".loss"
+# exp3
+id = "LSUV_dropout"
+accs_batch_path_LSUV_dropout = id + "/" + "all_cnn_accs_batch_" + id + ".acc"
+losses_batch_path_LSUV_dropout = id + "/" + "all_cnn_losses_batch_" + id + ".loss"
+
+id = "LSUV_no_dropout"
+accs_batch_path_LSUV_no_dropout = id + "/" + "all_cnn_accs_batch_" + id + ".acc"
+losses_batch_path_LSUV_no_dropout = id + "/" + "all_cnn_losses_batch_" + id + ".loss"
+
+# id = "LSUV_norm_shift_flip_zca"
+# accs_batch_path_LSUV_norm_shift_flip_zca = id + "/" + "all_cnn_accs_batch_" + id + ".acc"
+# losses_batch_path_LSUV_norm_shift_flip_zca = id + "/" + "all_cnn_losses_batch_" + id + ".loss"
 
 
-with open(accs_batch_path_LSUV_nopp, "rb") as fp:
-    accs_batch_LSUV_nopp = pickle.load(fp)
+with open(accs_batch_path_LSUV_dropout, "rb") as fp:
+    accs_batch_LSUV_dropout = pickle.load(fp)
 
-with open(losses_batch_path_LSUV_nopp, "rb") as fp:
-    losses_batch_LSUV_nopp = pickle.load(fp)
+with open(losses_batch_path_LSUV_dropout, "rb") as fp:
+    losses_batch_LSUV_dropout = pickle.load(fp)
 
-with open(accs_batch_path_LSUV_nsf, "rb") as fp:
-    accs_batch_LSUV_nsf = pickle.load(fp)
+with open(accs_batch_path_LSUV_no_dropout, "rb") as fp:
+    accs_batch_LSUV_no_dropout = pickle.load(fp)
 
-with open(losses_batch_path_LSUV_nsf, "rb") as fp:
-    losses_batch_LSUV_nsf = pickle.load(fp)
+with open(losses_batch_path_LSUV_no_dropout, "rb") as fp:
+    losses_batch_LSUV_no_dropout = pickle.load(fp)
 
-with open(accs_batch_path_LSUV_norm_shift_flip_zca, "rb") as fp:
-    accs_batch_LSUV_norm_shift_flip_zca = pickle.load(fp)
-
-with open(losses_batch_path_LSUV_norm_shift_flip_zca, "rb") as fp:
-    losses_batch_LSUV_norm_shift_flip_zca = pickle.load(fp)
+# with open(accs_batch_path_LSUV_norm_shift_flip_zca, "rb") as fp:
+#     accs_batch_LSUV_norm_shift_flip_zca = pickle.load(fp)
+#
+# with open(losses_batch_path_LSUV_norm_shift_flip_zca, "rb") as fp:
+#     losses_batch_LSUV_norm_shift_flip_zca = pickle.load(fp)
 
 # acc
 fig = plt.figure(1, figsize=(40, 10))
-plt.plot(range(0, len(accs_batch_LSUV_nopp))[0:-1:5], accs_batch_LSUV_nopp[0:-1:5])
-plt.plot(range(0, len(accs_batch_LSUV_nsf))[0:-1:5], accs_batch_LSUV_nsf[0:-1:5])
-plt.plot(range(0, len(accs_batch_LSUV_norm_shift_flip_zca))[0:-1:5], accs_batch_LSUV_norm_shift_flip_zca[0:-1:5])
+plt.plot(range(0, len(accs_batch_LSUV_dropout))[0:-1:5], accs_batch_LSUV_dropout[0:-1:5])
+plt.plot(range(0, len(accs_batch_LSUV_no_dropout))[0:-1:5], accs_batch_LSUV_no_dropout[0:-1:5])
+# plt.plot(range(0, len(accs_batch_LSUV_norm_shift_flip_zca))[0:-1:5], accs_batch_LSUV_norm_shift_flip_zca[0:-1:5])
 # plt.xticks(range(0, len(accs_batch_LSUV_nopp))[0:-1:5], range(0, len(accs_batch_LSUV_nopp))[0:-1:500])
-plt.title('Exp2: model accuracy per batch')
+plt.title('Exp3: model accuracy per batch')
 plt.ylabel('acc')
 plt.xlabel('batch')
-plt.legend(['no preprocessing', 'shift, flip and normalization', 'shift, flip, normalization and zca whitening'], loc='upper left')
+plt.legend(['dropout', 'no dropout'], loc='upper left')
 # plt.show()
-plt.savefig("exp2_acc.png")
+plt.savefig("exp3_acc.png")
 
 
 # loss
 fig = plt.figure(2, figsize=(40, 10))
-plt.plot(range(0, len(losses_batch_LSUV_nopp))[0:-1:5], losses_batch_LSUV_nopp[0:-1:5])
-plt.plot(range(0, len(losses_batch_LSUV_nsf))[0:-1:5], losses_batch_LSUV_nsf[0:-1:5])
-plt.plot(range(0, len(losses_batch_LSUV_norm_shift_flip_zca))[0:-1:5], losses_batch_LSUV_norm_shift_flip_zca[0:-1:5])
+plt.plot(range(0, len(losses_batch_LSUV_dropout))[0:-1:5], losses_batch_LSUV_dropout[0:-1:5])
+plt.plot(range(0, len(losses_batch_LSUV_no_dropout))[0:-1:5], losses_batch_LSUV_no_dropout[0:-1:5])
+# plt.plot(range(0, len(losses_batch_LSUV_norm_shift_flip_zca))[0:-1:5], losses_batch_LSUV_norm_shift_flip_zca[0:-1:5])
 # plt.xticks(range(0, len(losses_batch_LSUV_nopp))[0:-1:5], range(0, len(losses_batch_LSUV_nopp))[0:-1:5])
-plt.title('Exp2: model loss per batch')
+plt.title('Exp3: model loss per batch')
 plt.ylabel('loss')
 plt.xlabel('batch')
-plt.legend(['no preprocessing', 'shift, flip and normalization', 'shift, flip, normalization and zca whitening'], loc='upper left')
+plt.legend(['dropout', 'no dropout'], loc='upper left')
 # plt.show()
-plt.savefig("exp2_loss.png")
-
+plt.savefig("exp3_loss.png")
