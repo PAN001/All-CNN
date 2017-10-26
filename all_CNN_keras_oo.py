bd299d649e349ab3ad3677015606ec81bbe0049e
PAN001 <pan.atopos@gmail.com>
2017-10-25 21:34:12 -0400

    update

--- all_CNN_keras_oo.py
+++ all_CNN_keras_oo.py
@@ -16,6 +16,7 @@ from shuffle import *
 from LSUV import *
 import argparse
 import pandas
+import matplotlib.pyplot as plt
 # import cv2
 import numpy as np
 
@@ -117,7 +118,7 @@ datagen_train = ImageDataGenerator(
     featurewise_std_normalization=False,  # divide inputs by std of the dataset
     samplewise_std_normalization=False,  # divide each input by its std
     zca_whitening=True,  # apply ZCA whitening
-    rotation_range=20,  # randomly rotate images in the range (degrees, 0 to 180)
+    rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
     width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
     height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
     horizontal_flip=True,  # randomly flip images
@@ -126,10 +127,10 @@ datagen_train = ImageDataGenerator(
 datagen_test = ImageDataGenerator(
     featurewise_center=False,
     # set input mean to 0 over the dataset (featurewise subtract the mean image from every image in the dataset)
-    samplewise_center=True,  # set each sample mean to 0 (for each image each channel)
+    samplewise_center=False,  # set each sample mean to 0 (for each image each channel)
     featurewise_std_normalization=False,  # divide inputs by std of the dataset
-    samplewise_std_normalization=True,  # divide each input by its std
-    zca_whitening=False # apply ZCA whitening)
+    samplewise_std_normalization=False,  # divide each input by its std
+    zca_whitening=True # apply ZCA whitening)
 )
 
 # initialize the model
@@ -177,6 +178,23 @@ if is_training:
     pandas.DataFrame(history_callback.history).to_csv(history_path)
     model.save(new_final_weights_path)
 
+    # summarize history for accuracy
+    plt.plot(history_callback.history['acc'])
+    plt.plot(history_callback.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    
+    # summarize history for loss
+    plt.plot(history_callback.history['loss'])
+    plt.plot(history_callback.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
 else:
     print("read weights from the pretrained")
     model.load_weights(old_weights_path)
