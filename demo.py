8f6c3f23f6c40d7b502909c135efa7ed05ce9235
PAN001 <pan.atopos@gmail.com>
2017-10-26 22:24:08 -0400

    refactor

--- demo.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import numpy as np
-
-# l = [0.111223123231,-.123123123123]
-# with open("test.txt", "wb") as fp:
-#     pickle.dump(l, fp)
-
-with open("glorot_uniform/all_cnn_val_accs_epoch_glorot_uniform.acc", "rb") as fp:
-    b = pickle.load(fp)
