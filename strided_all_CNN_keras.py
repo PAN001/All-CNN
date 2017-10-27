aa64d8d516d6fd883ae9819d88e6175f22336630
PAN001 <pan.atopos@gmail.com>
2017-10-26 20:26:03 -0400

    update

--- strided_all_CNN_keras.py
+++ strided_all_CNN_keras.py
@@ -223,11 +223,11 @@ def main():
         featurewise_std_normalization=False,  # divide inputs by std of the dataset
         samplewise_std_normalization=False,  # divide each input by its std
         zca_whitening=False,  # apply ZCA whitening
-        # rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
         width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
         height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
         horizontal_flip=True,  # randomly flip images
-        vertical_flip=True
+        vertical_flip=False
     )
 
     datagen_test = ImageDataGenerator(
